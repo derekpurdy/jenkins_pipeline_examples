ea1ba92f559827edf28a641ae1cfcd60ea84d3ee
Derek Purdy <10130789+derekpurdy@users.noreply.github.com>
2024-09-06 14:16:55 -0400

    Update readFromPropertiesFile.groovy

--- readFromPropertiesFile.groovy
+++ readFromPropertiesFile.groovy
@@ -30,18 +30,4 @@ pipeline {
             }
         }
     }
-    post {
-        always {
-            echo "========always========"
-        }
-        success {
-            echo "========pipeline executed successfully ========"
-        }
-        failure {
-            echo "========pipeline execution failed========"
-        }
-        cleanup {
-            cleanWs()
-        }
-    }
 }

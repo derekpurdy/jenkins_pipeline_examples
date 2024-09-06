e72a4e219ced62588f32ceba77f59ab0c53f988f
Derek Purdy <10130789+derekpurdy@users.noreply.github.com>
2024-09-06 15:22:27 -0400

    Update ParameterizedTest.groovy

--- ParameterizedTest.groovy
+++ ParameterizedTest.groovy
@@ -6,8 +6,7 @@ pipeline {
     parameters {
         string(name: "BLAH1", defaultValue: "NOT_VALID", trim: true, description: "BLAH1 Value")
         string(name: "BLAH2", defaultValue: "NOT_VALID", trim: true, description: "BLAH2 Value")
-        string(name: "BLAH3", defaultValue: "NOT_VALID", trim: true, description: "BLAH3 Value")
-        string(name: "BLAH4", defaultValue: "NOT_VALID", trim: true, description: "BLAH4 Value")
+        choice(name: 'BLAH3', choices: ['NOT_VALID', 'VALID'], description: 'BLAH5 Value') 
     }
 
     stages {
@@ -16,7 +15,6 @@ pipeline {
                 echo "Blah1: $params.BLAH1"
                 echo "Blah2: $params.BLAH2"
                 echo "Blah3: $params.BLAH3"
-                echo "Blah4: $params.BLAH4"
             }
         }
     }
